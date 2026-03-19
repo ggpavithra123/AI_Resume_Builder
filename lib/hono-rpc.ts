63a9bac0b6c75737224e6f404e689658f2e3a8ec
ggpavithra123 <ggpavi12@gmail.com>
2026-03-19 10:44:03 +0530

    fix TypeScript unknown type in hono-rpc

--- lib/hono-rpc.ts
+++ lib/hono-rpc.ts
@@ -1,6 +1,8 @@
 import { hc } from "hono/client";
 import { AppType } from "@/app/api/[[...route]]/route";
 
-export const client = hc<AppType>("https://ai-resume-builder-xnok.vercel.app");
+export const client = hc<AppType>("https://ai-resume-builder-xnok.vercel.app") as {
+  api: AppType;
+};
 
 export const api = client.api;
