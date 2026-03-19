cc444c13a34e5ca8ff556809dd8a36a6118eecd6
ggpavithra123 <ggpavi12@gmail.com>
2026-03-19 10:39:25 +0530

    link updation

--- lib/hono-rpc.ts
+++ lib/hono-rpc.ts
@@ -1,6 +1,6 @@
 import { hc } from "hono/client";
 import { AppType } from "@/app/api/[[...route]]/route";
 
-export const client = hc<AppType>("http://localhost:3001");
+export const client = hc<AppType>("https://ai-resume-builder-xnok.vercel.app");
 
 export const api = client.api;
